4ecb568c53479776e68e051bbe15c0225da01651
SalmanHossen <hossainsalman873@gmail.com>
2024-11-13 18:00:02 +0600

    done all routing

--- client-side/src/pages/About.jsx
+++ client-side/src/pages/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const SignUp = () => {
+const About = () => {
   return (
     <div>
-      SignUp
+      About
     </div>
   )
 }
 
-export default SignUp
+export default About
