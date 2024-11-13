9354fd44c921d8e230698e2d44d707b1a5c2742a
SalmanHossen <hossainsalman873@gmail.com>
2024-11-13 18:53:33 +0600

    fix navbar issues

--- client-side/src/components/Header.jsx
+++ client-side/src/components/Header.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return (
-    <div className='bg-green-500'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-         <Link to='/'>
-            <h1 className='font-bold'>Auth App</h1>
-         </Link>
-         <ul className='flex gap-4 '>
-            <Link to='/'><li>Home</li></Link>
-            <Link to='/about'><li>About</li></Link>
-            <Link to='/sign-in'><li>Sign In</li></Link>  
-         </ul>
+   const [isOpen, setIsOpen] = useState(false);
+
+   const toggleMenu = () => {
+      setIsOpen(!isOpen);
+   };
+
+   return (
+      <div className="bg-green-500">
+         <div className="flex justify-between items-center mx-auto p-3 max-w-6xl">
+            <Link to="/">
+               <h1 className="font-bold">Auth App</h1>
+            </Link>
+            <button
+               className="block md:hidden text-white"
+               onClick={toggleMenu}
+            >
+            </button>
+            <ul
+               className={`flex flex-col md:flex-row items-center gap-4 absolute md:static bg-green-500 w-full md:w-auto left-0 md:left-auto p-3 md:p-0 transition-transform duration-300 ${isOpen ? 'top-12' : '-top-full'
+                  }`}
+            >
+               <Link to="/" onClick={() => setIsOpen(false)}>
+                  <li>Home</li>
+               </Link>
+               <Link to="/about" onClick={() => setIsOpen(false)}>
+                  <li>About</li>
+               </Link>
+               <Link to="/sign-in" onClick={() => setIsOpen(false)}>
+                  <li>Sign In</li>
+               </Link>
+            </ul>
+         </div>
       </div>
-    </div>
-  )
+   );
 }
 
-export default Header
+export default Header;
